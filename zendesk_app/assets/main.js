f7aa06090295b740f3ae88fecd566c5e5032cbee
Zewed <dewez.antoine2@gmail.com>
2024-11-28 16:14:16 +0100

    wip

--- zendesk_app/assets/main.js
+++ zendesk_app/assets/main.js
@@ -4,8 +4,6 @@ client.invoke("resize", { width: "100%", height: "600px" });
 const quivrApiKeyPromise = client.metadata().then(function (metadata) {
   return metadata.settings.quivr_api_key;
 });
-// Function Definition
-//--------------------
 
 function getInput(client) {
   return client.get("ticket.comment").then(function (data) {
@@ -20,7 +18,7 @@ function getHistoric(client) {
   });
 }
 
-async function getNewChat(client) {
+async function getNewChat() {
   const response = await fetch("https://api.quivr.app/chat", {
     method: "POST",
     headers: {
@@ -73,7 +71,6 @@ async function getQuivrResponse(prompt, chat_id) {
     result += decoder.decode(value, { stream: true });
   }
 
-  // Extract the final answer from the streamed response
   const matches = result.match(/data: (.+?)(?=data:|$)/g);
   const finalAnswer = matches
     .map((match) => JSON.parse(match.replace("data: ", "")).assistant)
@@ -83,10 +80,8 @@ async function getQuivrResponse(prompt, chat_id) {
 }
 
 async function reformulate(client, instruction) {
-  console.log(instruction);
   const historic = await getHistoric(client);
   const input = await getInput(client);
-  console.log(input);
 
   chat_id = await getNewChat(client);
   const clientName = await getRequesterName(client);
@@ -97,6 +92,7 @@ async function reformulate(client, instruction) {
     Here is the tone instruction:\n${instruction}\n
     Stick to the agent content. Client name : ${clientName} \nAgent Name : ${agentName} Here is the chat history: \n\n
     ${historic}\n\nReformulate this agent draft answer \n: ${input} \n\n Respond only with the reformulation in the same language as the draft answer : `;
+
   return getQuivrResponse(prompt, chat_id);
 }
 
@@ -116,36 +112,47 @@ function getRequesterName(client) {
   });
 }
 
-//--------------------
-
 document.addEventListener("DOMContentLoaded", () => {
   let clicked = false;
+
+  function adjustTextareaHeight(textarea) {
+    textarea.style.height = "26px";
+    textarea.style.height = textarea.scrollHeight + "px";
+  }
+
   function loadComponent(placeholderId, filePath, callback) {
     const placeholder = document.getElementById(placeholderId);
     fetch(filePath)
       .then((response) => response.text())
       .then((data) => {
         placeholder.innerHTML = data;
-        if (callback) callback(); // Exécuter une fonction après le chargement
+        if (callback) callback();
       })
       .catch((error) =>
         console.error(`Erreur lors du chargement de ${filePath}:`, error)
       );
   }
 
-  // Charger les composants
   loadComponent(
     "textarea-placeholder",
     "components/textarea/textarea.html",
     () => {
       const textarea = document.getElementById("instruction");
-      console.info(textarea);
       if (textarea) {
-        textarea.addEventListener("input", function () {
-          this.style.height = ""; // Reset height
-          this.style.height = this.scrollHeight + 100 + "px"; // Adjust height
-          console.log(this.scrollHeight);
+        textarea.addEventListener("change", function () {
+          adjustTextareaHeight(this);
         });
+        textarea.addEventListener("input", function () {
+          adjustTextareaHeight(this);
+        });
+
+        setTimeout(() => {
+          const textarea = document.getElementById("instruction");
+          textarea.value =
+            "Vous êtes un assistant attentionné de LocService, et votre objectif est de satisfaire la demande du client.";
+          const event = new Event("change");
+          textarea.dispatchEvent(event);
+        }, 1000);
       } else {
         console.warn("Textarea with ID 'instruction' not found.");
       }
@@ -154,16 +161,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
   loadComponent(
     "response-block-placeholder",
-    "components/response_block/response_block.html",
-    () => {
-      console.log("Response block loaded.");
-    }
+    "components/response_block/response_block.html"
   );
 
   loadComponent("button-placeholder", "components/button/button.html", () => {
     const button = document.getElementById("submit");
     const pasteButton = document.getElementById("paste");
-    const buttonTextWrapper = document.getElementById("button-text-wrapper");
+    const buttonTextWrapper = document.getElementById("button-icon");
     const buttonText = document.getElementById("button-text");
     const responseWrapper = document.getElementById("response_block_wrapper");
     const responseText = document.getElementById("quivr_response");
@@ -172,9 +176,8 @@ document.addEventListener("DOMContentLoaded", () => {
     if (button) {
       button.addEventListener("click", async () => {
         try {
-          console.log("Reformulating text...");
-          loader.style.display = "inline-block"; // Show loader
-          buttonTextWrapper.style.display = "none"; // Hide button text
+          loader.style.display = "block";
+          buttonTextWrapper.style.display = "none";
           button.disabled = true;
           const instruction = document.getElementById("instruction").value;
 
@@ -193,8 +196,8 @@ document.addEventListener("DOMContentLoaded", () => {
           responseWrapper.textContent =
             "An error occurred while reformulating the text.";
         } finally {
-          loader.style.display = "none"; // Hide loader
-          buttonTextWrapper.style.display = "inline"; // Show button text again
+          loader.style.display = "none";
+          buttonTextWrapper.style.display = "inline";
           button.disabled = false;
         }
       });
@@ -206,7 +209,6 @@ document.addEventListener("DOMContentLoaded", () => {
       pasteButton.addEventListener("click", async () => {
         try {
           const reformulatedText = responseText.innerHTML;
-          console.log("Pasting reformulated text in editor...");
           await pasteInEditor(client, reformulatedText);
         } catch (error) {
           console.error("Error pasting text in editor:", error);
