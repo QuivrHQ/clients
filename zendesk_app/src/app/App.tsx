23e3b4a218aa9936874fa4139655000e9be470e6
Zewed <dewez.antoine2@gmail.com>
2025-02-24 12:20:25 +0100

    done

--- zendesk_app/src/app/App.tsx
+++ zendesk_app/src/app/App.tsx
@@ -120,21 +120,33 @@ function App() {
       const ticketId = await getTicketId(client)
       const userInput = await getUserInput(client)
 
-      const result = await quivrService.executeZendeskTask(
+      await quivrService.executeZendeskTask(
         task,
         chatId,
         task === 'iterate' ? iterationRequest : agentPrompt,
         ticketId,
-        task === 'iterate' ? response : userInput
+        task === 'iterate' ? response : userInput,
+        (message: string) => {
+          if (!!message.length) {
+            setLoading(false)
+            clearInterval(loadingInterval)
+            if (isLoadingText()) {
+              setResponse(message)
+            } else {
+              setResponse((prevResponse) =>
+                prevResponse === '.' || prevResponse === '..' || prevResponse === '...'
+                  ? message.replace(/\\n/g, '\n').replace(/\n/g, '<br>') // On écrase le loadingText
+                  : prevResponse + message.replace(/\\n/g, '\n').replace(/\n/g, '<br>')
+              )
+            }
+          }
+        }
       )
-
-      setResponse(result.replace(/\\n/g, '\n').replace(/\n/g, '<br>'))
     } catch (error) {
-      console.error('Error rewriting response:', error)
+      console.info(error)
       setResponse('Error occurred while rewriting response.')
     } finally {
       clearInterval(loadingInterval)
-      setLoading(false)
     }
   }
 
