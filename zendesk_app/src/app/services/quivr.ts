bf1ba0e17ea44c8fa94a53d1ebfefcad8c32b252
Martin <martinthenot@gmail.com>
2025-04-14 18:33:02 +0200

    feat: plug rating endpoint to the feedback modal

--- zendesk_app/src/app/services/quivr.ts
+++ zendesk_app/src/app/services/quivr.ts
@@ -236,4 +236,23 @@ export class QuivrService {
       throw new Error('Failed to get auto draft')
     }
   }
+
+  async rateGeneratedAnswer(ticketId: string, ratingScore: number, ratingComment: string): Promise<void> {
+    await this.client.request({
+      url: `${this.apiUrl}/zendesk/ticket_answer/rating`,
+      type: 'PUT',
+      headers: {
+        Authorization: `Bearer ${this.quivrApiKey}`,
+        'Content-Type': 'application/json'
+      },
+      accepts: 'application/json',
+      data: JSON.stringify({
+        zendesk_ticket_id: ticketId,
+        support_agent_rating: {
+          score: ratingScore,
+          comment: ratingComment === '' ? null : ratingComment
+        }
+      })
+    })
+  }
 }
