75daaf32d13f36c824eb0853da395319982885d7
Zewed <dewez.antoine2@gmail.com>
2025-04-14 15:06:29 +0200

    wip

--- zendesk_app/src/app/components/FeedbackModal/FeedbackModal.tsx
+++ zendesk_app/src/app/components/FeedbackModal/FeedbackModal.tsx
@@ -1,9 +1,48 @@
-import { type JSX } from 'react'
-
+import { useState, type JSX } from 'react'
+import QuivrButton from '../../shared/components/QuivrButton/QuivrButton'
+import TextAreaInput from '../../shared/components/TextAreaInput/TextAreaInput'
 import styles from './FeedbackModal.module.scss'
 
 export const FeedbackModal = (): JSX.Element => {
-  return <div className={styles.main_container}>Feedback</div>
+  const [rating, setRating] = useState(0)
+  const [feedback, setFeedback] = useState('')
+
+  const handleStarClick = (value: number) => {
+    setRating(value)
+  }
+
+  return (
+    <div className={styles.main_container}>
+      <h2>Rate the relevance of the response</h2>
+      <div className={styles.stars_container}>
+        {[...Array(10)].map((_, index) => {
+          const starValue = index + 1
+          return (
+            <span
+              key={starValue}
+              className={`${styles.star} ${starValue <= rating ? styles.filled : ''}`}
+              onClick={() => handleStarClick(starValue)}
+            >
+              ★
+            </span>
+          )
+        })}
+      </div>
+      <TextAreaInput
+        label="Enter you feedback here"
+        inputValue={feedback}
+        setInputValue={() => setFeedback}
+      ></TextAreaInput>
+      <QuivrButton
+        label="Submit"
+        onClick={() => {
+          console.log('Feedback submitted:', { rating, feedback })
+        }}
+        color="primary"
+        iconName="send"
+      ></QuivrButton>
+    </div>
+  )
 }
 
 export default FeedbackModal
