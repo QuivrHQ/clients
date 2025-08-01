934e3e5c60818d18354d4577214078950b0947ec
Martin <martinthenot@gmail.com>
2025-08-01 15:53:08 +0200

    fix: feedbacks

--- freshdesk-app/server/server.js
+++ freshdesk-app/server/server.js
@@ -2,7 +2,7 @@ exports = {
   onAppInstallHandler: async function (payload) {
     console.log('onAppInstallHandler invoked with following data: \n', payload)
     try {
-      const response = await $request.invokeTemplate('createHelpdeskAccount', {
+      await $request.invokeTemplate('createHelpdeskAccount', {
         body: JSON.stringify({
           subdomain: payload.currentHost.endpoint_urls.freshdesk.split('//')[1],
           api_key: payload.iparams.freshdeskApiKey,
@@ -11,10 +11,8 @@ exports = {
           time_range: payload.iparams.historicalLookupWindow || 180
         })
       })
-      console.log('Helpdesk account created successfully: \n', response)
       renderData()
     } catch (error) {
-      console.error('Error creating helpdesk account: \n', error)
       renderData({ message: 'Failed to create helpdesk account' })
     }
   },
