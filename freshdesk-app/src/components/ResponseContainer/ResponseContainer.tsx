cca22f2ea8fe77bc4b5d7e1fba7981394473c0b3
Martin <martinthenot@gmail.com>
2025-08-06 11:34:39 +0200

    feat: create task button

--- freshdesk-app/src/components/ResponseContainer/ResponseContainer.tsx
+++ freshdesk-app/src/components/ResponseContainer/ResponseContainer.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react'
-import { FwButton, FwTextarea } from '@freshworks/crayons/react'
+import type { ElementRef } from 'react'
+import { FwButton, FwButtonGroup, FwIcon, FwListOptions, FwPopover, FwTextarea } from '@freshworks/crayons/react'
 import { useQuivrClient } from '../../hooks/useQuivrClient/useQuivrClient'
 import { useFreshdeskClient } from '../../context/FreshdeskClientContext/FreshdeskClientContext'
 import { useAccountConfigContext } from '../../context/AccountConfigContext/AccountConfigContext'
@@ -11,6 +12,8 @@ const ResponseContainer: React.FC = () => {
   const { accountConfig } = useAccountConfigContext()
   const [draft, setDraft] = useState<string | null>(null)
   const editorRef = useRef<HTMLTextAreaElement>(null)
+  const popRef = useRef<ElementRef<typeof FwPopover> | null>(null)
+  const listRef = useRef<ElementRef<typeof FwListOptions> | null>(null)
 
   useEffect(() => {
     if (!client || !accountConfig || draft) return
@@ -42,10 +45,27 @@ const ResponseContainer: React.FC = () => {
     loadDraft()
   }, [client, accountConfig])
 
+  const options = [
+    { value: 'Reformulate', text: 'Reformulate reply', hideTick: true },
+    { value: 'Correct', text: 'Correct reply', hideTick: true },
+    { value: 'Translate', text: 'Translate reply', hideTick: true },
+    { value: 'Summarize', text: 'Summarize ticket', hideTick: true }
+  ]
+  const handleSelect = async (e: CustomEvent<any>) => {
+    console.log('Selected:', e.detail?.value)
+    await popRef.current?.hide() // close after select
+    const selectedValue = await listRef.current?.getSelectedOptions()
+
+    console.log('Selected value:', selectedValue)
+    await listRef.current?.setSelectedOptions([])
+    const selectedValue2 = await listRef.current?.getSelectedOptions()
+
+    console.log('Selected value:', selectedValue2)
+  }
   return (
     <div className="fw-p-4 fw-gap-2 fw-flex fw-flex-column">
-      <div className="fw-flex">
-        <FwButton
+      <div className="fw-flex fw-justify-between">
+        {/*  <FwButton
           size="small"
           color="secondary"
           onClick={async () => {
@@ -57,7 +77,32 @@ const ResponseContainer: React.FC = () => {
           }}
         >
           Copy draft
-        </FwButton>
+        </FwButton> */}
+
+        <FwPopover ref={popRef} sameWidth trigger="click" variant="select">
+          <FwButtonGroup slot="popover-trigger">
+            <FwButton
+              color="primary"
+              disabled={false}
+              onClick={() => {
+                console.log('Generate draft')
+              }}
+            >
+              Generate draft
+            </FwButton>
+            <FwButton size="icon" disabled={false}>
+              <FwIcon name="chevron-down" slot="after-label" />
+            </FwButton>
+          </FwButtonGroup>
+
+          <FwListOptions
+            ref={listRef}
+            multiple={false}
+            slot="popover-content"
+            options={options}
+            onFwChange={handleSelect}
+          />
+        </FwPopover>
       </div>
       <FwTextarea ref={editorRef} rows={20} value={draft ?? ''} state="normal" />
     </div>
