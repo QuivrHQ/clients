898517fa6bdae8ee01a2d4c2b89b5756ff9ede2a
Martin <martinthenot@gmail.com>
2025-09-04 18:52:57 +0200

    feat: integrate new generate route in Zendesk

--- zendesk_app/src/app/constants/feature-flags.ts
+++ zendesk_app/src/app/constants/feature-flags.ts
@@ -1,5 +1,6 @@
 export const featureFlags = {
   AUTOSEND_FEEDBACK_MODAL: 'autosend-feedback-modal',
   AUTOSEND_FEEDBACK: 'autosend-feedback',
-  LOW_CONFIDENCE_WARNING: 'low-confidence-warning'
+  LOW_CONFIDENCE_WARNING: 'low-confidence-warning',
+  GENERATE_V2: 'generate-v2'
 }
